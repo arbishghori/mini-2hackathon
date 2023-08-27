aee52052fc20c68eb9125a61316f1c744f870abe
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-27 09:17:26 -0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+    window.location.href = "dash.html";
+  });
+
+
+function redirectToDash(){
+  window.location.href="dash.html";
+}
+setTimeout(redirectToDash,5000);
